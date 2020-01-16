98cd5a2dfae05fea79c2829da61d47aa6865951c
Daniel Kraft <d@domob.eu>
2020-01-16 17:17:29 +0100

    Implement configurable fee for names.
    Make names cost a fee for registration and update.  The concrete fee for
    an operation must be configured through the pallet's Trait (and may e.g.
    depend on the name length).  Also what happens with the fee needs to be
    configured on the Trait (e.g. burn it, distribute it to miners, send to
    a fixed "root" account, ...).

--- names/src/lib.rs
+++ names/src/lib.rs
@@ -31,9 +31,11 @@
 use frame_support::{
     decl_module, decl_storage, decl_event, ensure,
     dispatch::DispatchResult, dispatch::fmt::Debug,
+    traits::{Currency, ExistenceRequirement, WithdrawReason, WithdrawReasons},
 };
 use codec::{Decode, Encode, FullCodec};
 use system::ensure_signed;
+use sp_runtime::traits::CheckedSub;
 
 /// The pallet's configuration trait.
 pub trait Trait: system::Trait {
@@ -42,8 +44,21 @@ pub trait Trait: system::Trait {
     /// Type for name values.
     type Value: Clone + Debug + Default + Eq + FullCodec;
 
+    /// Type for currency operations (in order to pay for names).
+    type Currency: Currency<Self::AccountId>;
+
     /// The overarching event type.
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+
+    /// Computes and returns the currency fee the sender has to pay for
+    /// a certain operation.
+    fn get_name_fee(op: &Operation<Self>)
+        -> <Self::Currency as Currency<Self::AccountId>>::Balance;
+
+    /// "Takes ownership" of the fee paid for a name registration.  This
+    /// function can just do nothing to effectively burn the fee, it may
+    /// deposit it to a developer account, or it may give it out to miners.
+    fn deposit_fee(value: <Self::Currency as Currency<Self::AccountId>>::NegativeImbalance);
 }
 
 /// All data stored with a name in the database.
@@ -81,6 +96,9 @@ pub struct Operation<T: Trait> {
     sender: T::AccountId,
     /// The owner it is sent to.
     recipient: T::AccountId,
+
+    /// The name fee to pay.
+    fee: <T::Currency as Currency<T::AccountId>>::Balance,
 }
 
 decl_storage! {
@@ -99,7 +117,7 @@ decl_module! {
         pub fn update(origin, name: T::Name, value: T::Value) -> DispatchResult {
             let who = ensure_signed(origin)?;
             let data = Self::check_assuming_signed(who, name, Some(value), None)?;
-            Self::execute(data);
+            Self::execute(data)?;
             Ok(())
         }
 
@@ -109,7 +127,7 @@ decl_module! {
         pub fn transfer(origin, name: T::Name, recipient: T::AccountId) -> DispatchResult {
             let who = ensure_signed(origin)?;
             let data = Self::check_assuming_signed(who, name, None, Some(recipient))?;
-            Self::execute(data);
+            Self::execute(data)?;
             Ok(())
         }
 
@@ -117,6 +135,13 @@ decl_module! {
 }
 
 impl<T: Trait> Module<T> {
+
+    /// Returns a withdraw reasons value for the fee payment.
+    fn withdraw_reasons() -> WithdrawReasons {
+        let mut res = WithdrawReasons::none();
+        res.set(WithdrawReason::Fee);
+        res
+    }
 
     /// Checks if a name operation is valid, assuming that we already know
     /// it was signed by the given account.  Value and recipient are optional.
@@ -147,19 +172,48 @@ impl<T: Trait> Module<T> {
             Some(new_recipient) => new_recipient,
         };
 
-        Ok(Operation::<T> {
+        let mut op = Operation::<T> {
             operation: typ,
             name: name,
             value: value,
             sender: sender,
             recipient: recipient,
-        })
+            fee: <T::Currency as Currency<T::AccountId>>::Balance::default(),
+        };
+        op.fee = T::get_name_fee(&op);
+
+        /* Make sure that we can withdraw the name fee from the sender account.
+           Note that ensure_can_withdraw does not by itself verify the
+           amount against the free balance, but just that the new balance
+           satisfies all locks in place.  Thus we have to do that ourselves.  */
+        let new_balance = match T::Currency::free_balance(&op.sender).checked_sub(&op.fee) {
+            None => return Err("insufficient balance for name fee"),
+            Some(b) => b,
+        };
+        match T::Currency::ensure_can_withdraw(&op.sender, op.fee, Self::withdraw_reasons(), new_balance) {
+            Err(_) => return Err("cannot withdraw name fee from sender"),
+            Ok(_) => (),
+        }
+
+        Ok(op)
     }
 
     /// Executes the state change (and fires events) for a given name operation.
     /// This should be called after check_assuming_signed (passing its result),
     /// and when potential other checks have been done as well.
-    pub fn execute(op: Operation<T>) {
+    ///
+    /// This function may actually fail (return an error) if the fee withdrawal
+    /// is not possible.  This can happen if some funds were spent externally
+    /// between the call to check_assuming_signed and this function.  If that
+    /// happens, then execute will be a noop.
+    pub fn execute(op: Operation<T>) -> DispatchResult {
+        /* As the very first step, handle the name fee.  This makes sure
+           that if withdrawal fails, it will not cause any other changes.  */
+        let imbalance = T::Currency::withdraw(&op.sender, op.fee,
+                                              Self::withdraw_reasons(),
+                                              ExistenceRequirement::AllowDeath)?;
+        T::deposit_fee(imbalance);
+
         let data = NameData::<T> {
             value: op.value,
             owner: op.recipient,
@@ -174,6 +228,7 @@ impl<T: Trait> Module<T> {
         Self::deposit_event(RawEvent::NameUpdated(op.name.clone(), data.clone()));
 
         <Names<T>>::insert(op.name, data);
+        Ok(())
     }
 
 }
--- names/src/tests.rs
+++ names/src/tests.rs
@@ -6,7 +6,8 @@ use sp_core::H256;
 use frame_support::{
     impl_outer_event, impl_outer_origin, parameter_types,
     assert_noop, assert_ok,
-    dispatch::DispatchError::BadOrigin,
+    dispatch::DispatchError,
+    traits::{Imbalance, LockableCurrency, ReservableCurrency, WithdrawReasons},
     weights::Weight,
 };
 use system::{EventRecord, Phase};
@@ -19,9 +20,15 @@ impl_outer_origin! {
     pub enum Origin for Test {}
 }
 
+/// Account ID that receives name fees.
+const FEE_RECEIVER: u64 = 12345;
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Test;
 parameter_types! {
+    pub const ExistentialDeposit: u128 = 1000;
+    pub const TransferFee: u128 = 0;
+    pub const CreationFee: u128 = 0;
     pub const BlockHashCount: u64 = 250;
     pub const MaximumBlockWeight: Weight = 1024;
     pub const MaximumBlockLength: u32 = 2 * 1024;
@@ -45,20 +52,50 @@ impl system::Trait for Test {
     type Version = ();
     type ModuleToIndex = ();
 }
+impl balances::Trait for Test {
+    type Balance = u128;
+    type OnFreeBalanceZero = ();
+    type OnNewAccount = ();
+    type Event = TestEvent;
+    type DustRemoval = ();
+    type TransferPayment = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type TransferFee = ();
+    type CreationFee = ();
+}
 
 mod names {
     pub use crate::Event;
 }
 impl_outer_event! {
     pub enum TestEvent for Test {
+        balances<T>,
         names<T>,
     }
 }
 
 impl Trait for Test {
+
     type Name = u64;
     type Value = u64;
+
+    type Currency = Balances;
     type Event = TestEvent;
+
+    fn get_name_fee(op: &Operation<Self>) -> u128 {
+        match op.operation {
+            OperationType::Registration => 100,
+            OperationType::Update => 0,
+        }
+    }
+
+    fn deposit_fee(neg: <Self::Currency as Currency<u64>>::NegativeImbalance) {
+        let value = neg.peek();
+        let pos = Balances::deposit_creating(&FEE_RECEIVER, value);
+        let result = pos.offset(neg).ok().expect("fee balances offset failed");
+        result.drop_zero().ok().expect("fee balances mismatch");
+    }
+
 }
 
 fn new_test_ext() -> sp_io::TestExternalities {
@@ -66,7 +103,21 @@ fn new_test_ext() -> sp_io::TestExternalities {
 }
 
 type System = system::Module<Test>;
+type Balances = balances::Module<Test>;
 type Mod = Module<Test>;
+
+fn add_balance(account: u64, value: u128) {
+    let _ = Balances::deposit_creating(&account, value);
+}
+
+/// Asserts that the current total balance of the given account matches
+/// the expected value.  By having this as a convenience function, we can
+/// easily pass in the account as literal integer (while calling
+/// Balances::total_balance directly would mean we need a variable that we
+/// can take a reference of).
+fn expect_balance(account: u64, expected: u128) {
+    assert_eq!(Balances::total_balance(&account), expected);
+}
 
 /* ************************************************************************** */
 
@@ -79,36 +130,49 @@ mod extrinsics {
     #[test]
     fn registration() {
         new_test_ext().execute_with(|| {
+            add_balance(FEE_RECEIVER, 1000);
+            add_balance(10, 5000);
             assert_eq!(<Names<Test>>::get(100), None);
             assert_ok!(Mod::update(Origin::signed(10), 100, 42));
-            assert_noop!(Mod::update(Origin::ROOT, 200, 30), BadOrigin);
+            assert_noop!(Mod::update(Origin::ROOT, 200, 30),
+                         DispatchError::BadOrigin);
             assert_eq!(<Names<Test>>::get(100), Some(NameData::<Test> {
                 value: 42,
                 owner: 10,
             }));
             assert_eq!(<Names<Test>>::get(200), None);
+            expect_balance(FEE_RECEIVER, 1100);
+            expect_balance(10, 4900);
         });
     }
 
     #[test]
     fn update() {
         new_test_ext().execute_with(|| {
+            add_balance(FEE_RECEIVER, 1000);
+            add_balance(10, 5000);
             assert_ok!(Mod::update(Origin::signed(10), 100, 42));
             assert_ok!(Mod::update(Origin::signed(10), 100, 50));
             assert_noop!(Mod::update(Origin::signed(20), 100, 666),
                          "non-owner name update");
-            assert_noop!(Mod::update(Origin::ROOT, 100, 666), BadOrigin);
+            assert_noop!(Mod::update(Origin::ROOT, 100, 666),
+                         DispatchError::BadOrigin);
             assert_eq!(<Names<Test>>::get(100), Some(NameData::<Test> {
                 value: 50,
                 owner: 10,
             }));
+            expect_balance(FEE_RECEIVER, 1100);
+            expect_balance(10, 4900);
         });
     }
 
     #[test]
     fn transfer() {
         new_test_ext().execute_with(|| {
-            assert_noop!(Mod::transfer(Origin::ROOT, 100, 42), BadOrigin);
+            add_balance(FEE_RECEIVER, 1000);
+            add_balance(10, 5000);
+            assert_noop!(Mod::transfer(Origin::ROOT, 100, 42),
+                         DispatchError::BadOrigin);
             assert_ok!(Mod::transfer(Origin::signed(10), 100, 20));
             assert_noop!(Mod::transfer(Origin::signed(10), 100, 30),
                          "non-owner name update");
@@ -118,6 +182,8 @@ mod extrinsics {
                 value: 99,
                 owner: 40,
             }));
+            expect_balance(FEE_RECEIVER, 1100);
+            expect_balance(10, 4900);
         });
     }
 
@@ -132,12 +198,14 @@ mod check_function {
     #[test]
     fn registration_defaults() {
         new_test_ext().execute_with(|| {
+            add_balance(10, 5000);
             assert_ok!(Mod::check_assuming_signed(10, 100, None, None), Operation {
                 operation: OperationType::Registration,
                 name: 100,
                 value: 0,
                 sender: 10,
                 recipient: 10,
+                fee: 100,
             });
         });
     }
@@ -145,12 +213,14 @@ mod check_function {
     #[test]
     fn registration_with_values() {
         new_test_ext().execute_with(|| {
+            add_balance(10, 5000);
             assert_ok!(Mod::check_assuming_signed(10, 100, Some(42), Some(20)), Operation {
                 operation: OperationType::Registration,
                 name: 100,
                 value: 42,
                 sender: 10,
                 recipient: 20,
+                fee: 100,
             });
         });
     }
@@ -179,6 +249,7 @@ mod check_function {
                 value: 42,
                 sender: 10,
                 recipient: 10,
+                fee: 0,
             });
         });
     }
@@ -196,6 +267,46 @@ mod check_function {
                 value: 50,
                 sender: 10,
                 recipient: 20,
+                fee: 0,
+            });
+        });
+    }
+
+    #[test]
+    fn balance_checks() {
+        new_test_ext().execute_with(|| {
+            assert_noop!(Mod::check_assuming_signed(10, 100, None, None),
+                         "insufficient balance for name fee");
+
+            let locked_account = 20;
+            add_balance(locked_account, 5000);
+            <Balances as LockableCurrency<u64>>::set_lock(
+                [1, 2, 3, 4, 5, 6, 7, 8], &locked_account,
+                4901, 100, WithdrawReasons::all());
+            assert_noop!(Mod::check_assuming_signed(locked_account, 100, None, None),
+                         "cannot withdraw name fee from sender");
+
+            let reserved_account = 30;
+            add_balance(reserved_account, 5000);
+            assert_ok!(<Balances as ReservableCurrency<u64>>::reserve(
+                            &reserved_account, 4901));
+            assert_noop!(Mod::check_assuming_signed(reserved_account, 100, None, None),
+                         "insufficient balance for name fee");
+
+            let ok_account = 40;
+            add_balance(ok_account, 2100);
+            <Balances as LockableCurrency<u64>>::set_lock(
+                [1, 2, 3, 4, 5, 6, 7, 8], &ok_account,
+                1000, 100, WithdrawReasons::all());
+            assert_ok!(<Balances as ReservableCurrency<u64>>::reserve(
+                            &ok_account, 1000));
+            assert_ok!(Mod::check_assuming_signed(ok_account, 100, Some(50), Some(20)), Operation {
+                operation: OperationType::Registration,
+                name: 100,
+                value: 50,
+                sender: ok_account,
+                recipient: 20,
+                fee: 100,
             });
         });
     }
@@ -211,25 +322,27 @@ mod execute_function {
     #[test]
     fn updates_storage() {
         new_test_ext().execute_with(|| {
-            Mod::execute(Operation {
+            assert_ok!(Mod::execute(Operation {
                 operation: OperationType::Registration,
                 name: 100,
                 value: 42,
                 sender: 10,
                 recipient: 10,
-            });
+                fee: 0,
+            }));
             assert_eq!(<Names<Test>>::get(100), Some(NameData::<Test> {
                 value: 42,
                 owner: 10,
             }));
 
-            Mod::execute(Operation {
+            assert_ok!(Mod::execute(Operation {
                 operation: OperationType::Update,
                 name: 100,
                 value: 50,
                 sender: 10,
                 recipient: 20,
-            });
+                fee: 0,
+            }));
             assert_eq!(<Names<Test>>::get(100), Some(NameData::<Test> {
                 value: 50,
                 owner: 20,
@@ -238,24 +351,82 @@ mod execute_function {
     }
 
     #[test]
+    fn fee_handling() {
+        new_test_ext().execute_with(|| {
+            add_balance(FEE_RECEIVER, 1000);
+            add_balance(10, 5000);
+            assert_ok!(Mod::execute(Operation {
+                operation: OperationType::Registration,
+                name: 100,
+                value: 50,
+                sender: 10,
+                recipient: 10,
+                fee: 50,
+            }));
+            expect_balance(FEE_RECEIVER, 1050);
+            expect_balance(10, 4950);
+            assert_eq!(Balances::total_issuance(), 6000);
+
+            /* Verify that we get a noop if the withdrawal fails.  */
+            assert_noop!(Mod::execute(Operation {
+                operation: OperationType::Update,
+                name: 100,
+                value: 60,
+                sender: 10,
+                recipient: 20,
+                fee: 5000,
+            }), DispatchError::Module {
+                index: 0,
+                error: 3,
+                message: Some("InsufficientBalance"),
+            });
+
+            /* Process a situation where the account gets killed due
+               to falling below the existence minimum.  This will then
+               kill the account, effectively burning the remaining balance.  */
+            assert_ok!(Mod::execute(Operation {
+                operation: OperationType::Update,
+                name: 100,
+                value: 70,
+                sender: 10,
+                recipient: 10,
+                fee: 4000,
+            }));
+            assert_eq!(<Names<Test>>::get(100), Some(NameData::<Test> {
+                value: 70,
+                owner: 10,
+            }));
+            expect_balance(FEE_RECEIVER, 5050);
+            expect_balance(10, 0);
+            assert_eq!(Balances::total_issuance(), 5050);
+        });
+    }
+
+    #[test]
     fn events() {
         new_test_ext().execute_with(|| {
-            Mod::execute(Operation {
+            add_balance(FEE_RECEIVER, 1000);
+            add_balance(10, 5000);
+            let balance_events = System::events();
+
+            assert_ok!(Mod::execute(Operation {
                 operation: OperationType::Registration,
                 name: 100,
                 value: 42,
                 sender: 10,
                 recipient: 10,
-            });
-            Mod::execute(Operation {
+                fee: 0,
+            }));
+            assert_ok!(Mod::execute(Operation {
                 operation: OperationType::Update,
                 name: 100,
                 value: 50,
                 sender: 10,
                 recipient: 20,
-            });
+                fee: 0,
+            }));
 
-            assert_eq!(System::events(), vec![
+            let name_events = vec![
                 EventRecord {
                     phase: Phase::ApplyExtrinsic(0),
                     event: TestEvent::names(RawEvent::NameRegistered(100)),
@@ -277,7 +448,9 @@ mod execute_function {
                     })),
                     topics: vec![],
                 },
-            ]);
+            ];
+            assert_eq!(System::events(),
+                       [&balance_events[..], &name_events[..]].concat());
         });
     }
 
