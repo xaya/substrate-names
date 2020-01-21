0b342c6b365e6a7f7e867432e9a79257a00638b4
Daniel Kraft <d@domob.eu>
2020-01-21 12:42:10 +0100

    Polish rust docs.
    Update the names crate's rustdocs with proper Markdown and cross-references
    to other parts of the docs.

--- names/src/lib.rs
+++ names/src/lib.rs
@@ -1,32 +1,35 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// A pallet that defines a system to register and update names
-/// in a Substrate chain.  This provides (roughly) the functionality
-/// of the Namecoin blockchain.
-///
-/// The core concept is that of a "name".  This is some identifier (the exact
-/// type can be configured through the module's Trait), e.g. a human-readable
-/// name as string.  Each name is unique, and has an associated value and owner.
-/// Everyone can read the database of names, but only the owner can make
-/// changes to it.  This typically means changing the value to publish some
-/// data with the name, but the owner can also transfer names to a different
-/// owner.
-///
-/// Names are given out on a first come, first serve basis.  Each name that
-/// is not yet registered (and valid for the system) can be registered by
-/// any account (which may incur a fee for registration, and then maybe also
-/// for updates to the name).  Once registered, the name is owned by the
-/// account that first registered it.
-///
-/// After a certain number of blocks, names may expire and become usable again.
-/// By updating a name before the expiration, the current owner can keep
-/// ownership.
-///
-/// The names module defines basic extrinsics to perform name operations
-/// (register / update / transfer names) and events corresponding to changes
-/// in the name database.  But if custom logic needs to be applied in addition
-/// by the runtime, it may use the exposed functions check_assuming_signed
-/// and execute directly.
+//! A pallet that defines a system to register and update names
+//! in a Substrate chain.  This provides (roughly) the functionality
+//! of the [Namecoin](https://www.namecoin.org/) blockchain.
+//!
+//! The core concept is that of a *name*.  This is some identifier (the exact
+//! type can be configured through the module's [`Trait`](Trait)),
+//! e.g. a human-readable name as string.  Each name is unique, and has an
+//! associated value and owner.  Everyone can read the database of names, but
+//! only the owner can make changes to it.  This typically means changing the
+//! value to publish some data with the name, but the owner can also transfer
+//! names to a different owner.
+//!
+//! Names are given out on a *first come, first serve* basis.  Each name that
+//! is not yet registered (and valid for the system) can be registered by
+//! any account (which may incur a fee for registration, and then maybe also
+//! for updates to the name).  Once registered, the name is owned by the
+//! account that first registered it.
+//!
+//! After a certain number of blocks, names may expire and become usable again.
+//! By updating a name before the expiration, the current owner can keep
+//! ownership.
+//!
+//! The `names` module defines basic extrinsics to perform name operations
+//! ([register](Module::update) / [update](Module::update) /
+//! [transfer](Module::transfer) names) and [events](RawEvent) corresponding to
+//! changes in the name database.  But if custom logic needs to be applied in
+//! addition by the runtime, it may use the exposed functions
+//! [`check_assuming_signed`](Module::check_assuming_signed) and
+//! [`execute`](Module::execute) directly.  The name database can be accessed
+//! from external code by using [`lookup`](Module::lookup).
 
 use frame_support::{
     decl_module, decl_storage, decl_event, ensure,
@@ -43,7 +46,7 @@ pub trait Trait: system::Trait {
 
     /// Type for names.
     type Name: Clone + Debug + Default + Eq + FullCodec;
-    /// Type for name values.
+    /// Type for values associated to names.
     type Value: Clone + Debug + Default + Eq + FullCodec;
 
     /// Type for currency operations (in order to pay for names).
@@ -53,17 +56,17 @@ pub trait Trait: system::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 
     /// Computes and returns the currency fee the sender has to pay for
-    /// a certain operation.  If None is returned, it means that the
+    /// a certain operation.  If `None` is returned, it means that the
     /// operation is invalid (e.g. the name is too short).
     fn get_name_fee(op: &Operation<Self>)
         -> Option<<Self::Currency as Currency<Self::AccountId>>::Balance>;
 
-    /// For a given name operation, compute the number of blocks before the
-    /// name will expire again.  If None is returned, then the name will
+    /// For a given name operation, computes the number of blocks before the
+    /// name will expire again.  If `None` is returned, then the name will
     /// never expire.
     fn get_expiration(op: &Operation<Self>) -> Option<Self::BlockNumber>;
 
-    /// "Takes ownership" of the fee paid for a name registration.  This
+    /// "Takes ownership" of the fee paid for a name operation.  This
     /// function can just do nothing to effectively burn the fee, it may
     /// deposit it to a developer account, or it may give it out to miners.
     fn deposit_fee(value: <Self::Currency as Currency<Self::AccountId>>::NegativeImbalance);
@@ -78,14 +81,7 @@ pub struct NameData<T: Trait> {
     pub value: T::Value,
     /// The name's current owner.
     pub owner: T::AccountId,
-    /// The block number when the name expires or None if it does not expire.
-    /// While we also have an explicit index mapping block numbers to names
-    /// that expire, the value here is stored a) for informative purposes
-    /// (so one can query when a certain name expires), and b) so that it
-    /// can overrule the expiration index:  If a name is updated, then we
-    /// are not removing it from the old expiration index; instead, we will
-    /// simply not expire names when processing a the expiration index if their
-    /// value here does not match the one from the index.
+    /// The block number when the name expires or `None` if it does not expire.
     pub expiration: Option<T::BlockNumber>,
 }
 
@@ -93,13 +89,21 @@ pub struct NameData<T: Trait> {
 #[cfg_attr(feature = "std", derive(Debug))]
 #[derive(Eq, PartialEq)]
 pub enum OperationType {
+    /// This operation registers a name that does not exist yet.
     Registration,
+    /// This operation updates an existing name.
     Update,
 }
 
-/// All data necessary to actually perform a name operation.  This is returned
-/// by the validation function, and can then be passed to the execution function
-/// if a runtime wants to do its own logic in addition.
+/// All data necessary to actually perform a name operation.
+///
+/// This is returned by the
+/// [validation function](Module::check_assuming_signed), and can then
+/// be passed to the [execution function](Module::execute) if a runtime wants
+/// to do its own logic in addition.
+///
+/// A reference to an `Operation` struct is also passed to the [`Trait`](Trait)
+/// functions that need to determine e.g. the name fee for the operation.
 #[cfg_attr(feature = "std", derive(Debug))]
 #[derive(Eq, PartialEq)]
 pub struct Operation<T: Trait> {
@@ -107,7 +111,7 @@ pub struct Operation<T: Trait> {
     pub operation: OperationType,
     /// The name being operated on.
     pub name: T::Name,
-    /// The value it is being set to.
+    /// The value that is being associated to the name.
     pub value: T::Value,
 
     /// The sender of the name (who pays the name fee).
@@ -121,7 +125,7 @@ pub struct Operation<T: Trait> {
 
 decl_storage! {
     trait Store for Module<T: Trait> as TemplateModule {
-        /// The main name -> data mapping.
+        /// The main mapping from names to [associated data](NameData).
         Names get(lookup): map T::Name => Option<NameData<T>>;
         /// All names (as both the second key and the value) that may expire at
         /// the given block height (first key).  We use this so we can
@@ -137,9 +141,10 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         fn deposit_event() = default;
 
-        /// Tries to update a name with a given value.  If the name does not
-        /// exist yet, it will be created.  If the name exists, then only the
-        /// current owner can update it.
+        /// Tries to update a name with a given value.
+        ///
+        /// If the name does not exist yet, it will be created.  If the name
+        /// exists, then only the current owner can update it.
         pub fn update(origin, name: T::Name, value: T::Value) -> DispatchResult {
             let who = ensure_signed(origin)?;
             let data = Self::check_assuming_signed(who, name, Some(value), None)?;
@@ -147,9 +152,10 @@ decl_module! {
             Ok(())
         }
 
-        /// Tries to transfer a name to a given recipient.  If the name does
-        /// not exist, it will be registered directly to them with a default
-        /// value.
+        /// Tries to transfer a name to a given recipient.
+        ///
+        /// If the name does not exist, it will be registered directly to them
+        /// with a [default value](std::default::Default).
         pub fn transfer(origin, name: T::Name, recipient: T::AccountId) -> DispatchResult {
             let who = ensure_signed(origin)?;
             let data = Self::check_assuming_signed(who, name, None, Some(recipient))?;
@@ -178,14 +184,16 @@ impl<T: Trait> Module<T> {
     }
 
     /// Checks if a name operation is valid, assuming that we already know
-    /// it was signed by the given account.  Value and recipient are optional.
-    /// If the value is missing, we use the existing value or the default
-    /// value if the name does not exist yet.  If the recipient is missing,
-    /// we set it to the sender account.
+    /// it was signed by the given account.
+    ///
+    /// Value and recipient are optional.  If the value is missing, we use the
+    /// existing value or the [default value](std::default::Default) if the
+    /// name does not exist yet.  If the recipient is missing, we set it to
+    /// the `sender` account.
     ///
     /// This function returns either an error if the operation is not valid,
-    /// or the data that should be passed to execute later on if the transaction
-    /// is valid.
+    /// or the [data](Operation) that should be passed to
+    /// [`execute`](Module::execute) later on if the transaction is valid.
     pub fn check_assuming_signed(sender: T::AccountId, name: T::Name,
                                  value: Option<T::Value>,
                                  recipient: Option<T::AccountId>) -> Result<Operation<T>, &'static str> {
@@ -235,14 +243,18 @@ impl<T: Trait> Module<T> {
         Ok(op)
     }
 
-    /// Executes the state change (and fires events) for a given name operation.
-    /// This should be called after check_assuming_signed (passing its result),
-    /// and when potential other checks have been done as well.
+    /// Executes the state change (and fires events) for a given
+    /// [name operation](Operation).
+    ///
+    /// This should be called after
+    /// [`check_assuming_signed`](Module::check_assuming_signed) (passing its
+    /// result), and when potential other checks have been done as well.
     ///
     /// This function may actually fail (return an error) if the fee withdrawal
     /// is not possible.  This can happen if some funds were spent externally
-    /// between the call to check_assuming_signed and this function.  If that
-    /// happens, then execute will be a noop.
+    /// between the call to
+    /// [`check_assuming_signed`](Module::check_assuming_signed) and this
+    /// function.  If that happens, then `execute` will be a noop.
     pub fn execute(op: Operation<T>) -> DispatchResult {
         /* As the very first step, handle the name fee.  This makes sure
            that if withdrawal fails, it will not cause any other changes.  */
