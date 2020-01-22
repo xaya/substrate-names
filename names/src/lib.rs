f4660497d0a393c165228a9fd233a2cae60d1f82
Daniel Kraft <d@domob.eu>
2020-01-22 14:33:18 +0100

    Add copyright headers for names pallet.
    Add a copyright header (matching the GPLv3 checked in previously)
    for the main source files in the names pallet itself (which is what
    we wrote mostly by ourselves).

--- names/src/lib.rs
+++ names/src/lib.rs
@@ -1,3 +1,21 @@
+/*
+    names - a pallet for Substrate blockchains implementing naming
+    Copyright (C) 2020  Autonomous Worlds Ltd
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 //! A pallet that defines a system to register and update names
